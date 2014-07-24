275499b43887e54e79d428f7bb3f0ec85b7db49a
noob247365 <trickstertales365@gmail.com>
2014-07-23 22:02:56 -0700

    Added Crouching

---
+++ Assets/Scripts/CanStand.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Collider))]
+public class CanStand : MonoBehaviour {
+
+	public Collider player;
+	public bool ableToStand = true;
+
+	private int lvlLayer = 0;
+	private bool tryToStand = false;
+
+	// Use this for initialization
+	void Start () {
+		Physics.IgnoreCollision (collider, player, true);
+		ableToStand = true;
+		lvlLayer = LayerMask.NameToLayer ("Level");
+	}
+
+	void LateUpdate() {
+		if (tryToStand) {
+			ableToStand = true;
+			tryToStand = false;
+		}
+	}
+	
+	void OnTriggerStay(Collider collision) {
+		if (collision.gameObject.layer == lvlLayer) {
+			ableToStand = false;
+			tryToStand = false;
+		}
+	}
+
+	void OnTriggerExit() {
+		tryToStand = true;
+	}
+}
--- Assets/Scripts/FPSControl.cs
+++ Assets/Scripts/FPSControl.cs
@@ -4,28 +4,40 @@ using System.Collections;
 [RequireComponent (typeof(CharacterController))]
 public class FPSControl : MonoBehaviour {
 
-	public GameObject Xobject;
-	public GameObject Yobject;
-	public GameObject CrouchObject;
+	public Transform Xobject;
+	public Transform Yobject;
+	public GameObject CharModel;
+	public GameObject StandTrigger;
 
 	public float MouseSensitivity = 5.0f;
 	public float Yrange = 60.0f;
 
 	public float speed = 2.5f;
-	public float sprint = 1.75f;
+	public float sprintSpeed = 1.75f;
+	public float crouchSpeed = 0.5f;
 	public float jumpSpeed = 5.0f;
 
 	CharacterController cc;
 	float xAngle = 0;
 	float yAngle = 0;
 	float jumpVelocity = 0;
+	CanStand cs = null;
+	bool crouching = false;
+	bool standing = false;
+	float Hcrouch;
+	float Hstand;
 
 	// Use this for initialization
 	void Start () {
 		Screen.lockCursor = true;
 		cc = GetComponent<CharacterController> ();
 		if (Xobject != null)
-			xAngle = Xobject.transform.eulerAngles.y;
+			xAngle = Xobject.eulerAngles.y;
+		if (StandTrigger != null)
+			cs = StandTrigger.GetComponent<CanStand> ();
+
+		Hstand = transform.localScale.y;
+		Hcrouch = Hstand * 0.5f;
 	}
 	
 	// Update is called once per frame
@@ -45,16 +57,37 @@ public class FPSControl : MonoBehaviour {
 		yAngle = Mathf.Clamp (yAngle, -Yrange, Yrange);
 
 		if (Xobject)
-			Xobject.transform.localEulerAngles = new Vector3 (0, xAngle, 0);
+			Xobject.localEulerAngles = new Vector3 (0, xAngle, 0);
 		if (Yobject)
-			Yobject.transform.localEulerAngles = new Vector3 (-yAngle, 0, 0);
+			Yobject.localEulerAngles = new Vector3 (-yAngle, 0, 0);
 
 		float Xmov = Input.GetAxis ("Horizontal") * speed;
 		float Ymov = Input.GetAxis ("Vertical") * speed;
-		float sprintMov = (Input.GetAxis ("Sprint") != 0) ? sprint : 1;
+		float sprintMov = 1 + Input.GetAxis ("Sprint") * (sprintSpeed - 1);
+
+		if (Input.GetButton ("Crouch")) {
+			crouching = true;
+			standing = false;
+		} else {
+			if (crouching)
+				standing = true;
+			if (standing) {
+				if (cs == null || cs.ableToStand) {
+					standing = false;
+					crouching = false;
+				}
+			}
+		}
+
+		if (crouching) {
+			transform.localScale = new Vector3(1, Hcrouch, 1);
+			sprintMov = crouchSpeed;
+		} else {
+			transform.localScale = new Vector3(1, Hstand, 1);
+		}
 
 		if (cc.isGrounded) {
-			if (Input.GetButtonDown ("Jump"))
+			if (Input.GetButtonDown ("Jump") && !crouching)
 				jumpVelocity = jumpSpeed;
 			else
 				jumpVelocity = -0.01f;
